bc789e6540b24b2b5d9fb613ae9a5eba7f227e44
mirjung <mr.jung@lomin.ai>
2021-11-11 13:11:32 +0900

    UPDATE: ci test

--- server/db/database.py
+++ server/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://mir:wjdalfm1!@localhost/test_fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://test:test@localhost/test_fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
